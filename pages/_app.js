41b0bea34421706dd96a5f3b70127cf93e13eba8
GabTomiazzi <gptomiazzi@hotmail.com>
2021-07-13 23:23:16 -0300

    Adicionado font family Poppins

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import { AlurakutStyles } from '../src/lib/AlurakutCommons'
 
 const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
   /* Reset CSS */
   * {
     margin: 0;
@@ -10,7 +11,7 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    font-family: sans-serif;
+    font-family: 'Poppins',sans-serif;
     background-image: url('https://wallpaperaccess.com/full/1339588.jpg');
     background-repeat: round;
   }
